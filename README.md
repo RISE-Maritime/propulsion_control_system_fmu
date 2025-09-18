cf50adb0fdc4436998d5e1fd2cf60d0a6f8e8534
Fredrik Olsson <freol@outlook.com>
2025-09-18 09:06:04 +0200

    Initial commit for PropulsionControlSystem system

---
+++ README.md
@@ -0,0 +1,20 @@
+# propulsion-control-system-fmu
+
+This FMU implements a very simplistic propulsion control system for ships.
+
+## For development
+
+This is an example setup of using rust for implementing a model that is also cross-compiled and packaged as a multi-architecture FMU compatible with FMI 3.0.
+
+Use the devcontainer. Use cargo as you would in any other project, i.e. `cargo build`, `cargo test`.
+
+## For generating a release
+
+Run `build_fmu.sh`, it will:
+* Install `cross` is not already installed.
+* Compile the project for:
+  * x86_64-unknown-linux-gnu
+  * x86_64-pc-windows-gnu
+* Package the resulting shred libraries and the `modelDescription.xml` into a temporary folder structure adhearing to FMI 3.0.
+* Zip the temporary folder structure into an FMU.
+
---
+++ src/lib.rs
@@ -0,0 +1,184 @@
+pub use fmu_from_struct::prelude::*;
+
+#[derive(Debug, Default, Clone, Fmu)]
+#[fmi_version = 3]
+pub struct PropulsionControlSystem {
+    #[input]
+    pub minimum_consumption: f64, // t/24h
+    pub setpoint_consumption: f64, // t/24h
+    pub setpoint_speed: f64,       // knots
+    pub actual_consumption: f64,   // t/24h
+    pub actual_speed: f64,         // knots
+
+    #[output]
+    pub mode: i32, // 0=minimum consumption mode, 1=consumption mode, 2=speed mode
+    pub lever: f64,                                            // 0..1
+    pub actual_consumption_relative_minimum_consumption: f64,  // 0..1
+    pub actual_speed_relative_setpoint_speed: f64,             // 0..1
+    pub actual_consumption_relative_setpoint_consumption: f64, // 0..1
+}
+
+fn calculate_relative_value(actual: f64, setpoint: f64) -> f64 {
+    if actual == 0.0 {
+        0.0
+    } else {
+        (setpoint - actual) / actual
+    }
+}
+
+impl FmuFunctions for PropulsionControlSystem {
+    fn do_step(&mut self, _current_time: f64, _time_step: f64) {
+        // Calculate relative values
+        self.actual_consumption_relative_minimum_consumption =
+            calculate_relative_value(self.actual_consumption, self.minimum_consumption);
+        self.actual_consumption_relative_setpoint_consumption =
+            calculate_relative_value(self.actual_consumption, self.setpoint_consumption);
+        self.actual_speed_relative_setpoint_speed =
+            calculate_relative_value(self.actual_speed, self.setpoint_speed);
+
+        if self.actual_consumption_relative_minimum_consumption
+            > f64::min(
+                self.actual_consumption_relative_setpoint_consumption,
+                self.actual_speed_relative_setpoint_speed,
+            )
+        {
+            self.lever = self.lever + self.actual_consumption_relative_minimum_consumption;
+            self.mode = 0;
+        } else if self.actual_consumption_relative_setpoint_consumption
+            < self.actual_speed_relative_setpoint_speed
+        {
+            self.lever = self.lever + self.actual_consumption_relative_setpoint_consumption;
+            self.mode = 1;
+        } else {
+            self.lever = self.lever + self.actual_speed_relative_setpoint_speed;
+            self.mode = 2;
+        }
+
+        self.lever = f64::max(0.0, f64::min(1.0, self.lever));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_mode_0_minimum_consumption() {
+        let mut sys = PropulsionControlSystem {
+            minimum_consumption: 10.0,
+            setpoint_consumption: 20.0,
+            setpoint_speed: 10.0,
+            actual_consumption: 5.0,
+            actual_speed: 10.0,
+            lever: 0.5,
+            ..Default::default()
+        };
+
+        sys.do_step(0.0, 1.0);
+        assert_eq!(sys.mode, 0); // Minimum consumption mode
+        assert!(sys.lever > 0.5);
+    }
+
+    #[test]
+    fn test_mode_1_consumption() {
+        let mut sys = PropulsionControlSystem {
+            minimum_consumption: 10.0,
+            setpoint_consumption: 20.0,
+            setpoint_speed: 10.0,
+            actual_consumption: 22.0,
+            actual_speed: 10.0,
+            lever: 0.5,
+            ..Default::default()
+        };
+
+        sys.do_step(0.0, 1.0);
+        assert_eq!(sys.mode, 1); // Consumption mode
+        assert!(sys.lever < 0.5);
+    }
+
+    #[test]
+    fn test_mode_2_speed() {
+        let mut sys = PropulsionControlSystem {
+            minimum_consumption: 10.0,
+            setpoint_consumption: 20.0,
+            setpoint_speed: 10.0,
+            actual_consumption: 18.0,
+            actual_speed: 11.0,
+            lever: 0.5,
+            ..Default::default()
+        };
+
+        sys.do_step(0.0, 1.0);
+        assert_eq!(sys.mode, 2); // Speed mode
+        assert!(sys.lever < 0.5);
+    }
+
+    #[test]
+    fn test_mode_0_minimum_consumption_no_change() {
+        let mut sys = PropulsionControlSystem {
+            minimum_consumption: 10.0,
+            setpoint_consumption: 20.0,
+            setpoint_speed: 10.0,
+            actual_consumption: 10.0,
+            actual_speed: 10.0,
+            lever: 0.5,
+            ..Default::default()
+        };
+
+        sys.do_step(0.0, 1.0);
+        assert_eq!(sys.mode, 2); // This will go into speed mode since comparisons are made  not including  equalities
+        assert!(sys.lever == 0.5);
+    }
+
+    #[test]
+    fn test_mode_1_consumption_no_change() {
+        let mut sys = PropulsionControlSystem {
+            minimum_consumption: 10.0,
+            setpoint_consumption: 20.0,
+            setpoint_speed: 10.0,
+            actual_consumption: 20.0,
+            actual_speed: 10.0,
+            lever: 0.5,
+            ..Default::default()
+        };
+
+        sys.do_step(0.0, 1.0);
+        assert_eq!(sys.mode, 2); // This will go into speed mode since comparisons are made  not including  equalities
+        assert!(sys.lever == 0.5);
+    }
+
+    #[test]
+    fn test_mode_priority() {
+        let mut sys = PropulsionControlSystem {
+            minimum_consumption: 5.0,
+            setpoint_consumption: 10.0,
+            setpoint_speed: 10.0,
+            actual_consumption: 9.0,
+            actual_speed: 11.0,
+            lever: 0.5,
+            ..Default::default()
+        };
+
+        sys.do_step(0.0, 1.0);
+        assert_eq!(sys.mode, 2);
+        assert!(sys.lever < 0.5);
+    }
+
+    #[test]
+    fn test_infinite_speed_setpoint_results_in_consumption_mode() {
+        let mut sys = PropulsionControlSystem {
+            minimum_consumption: 5.0,
+            setpoint_consumption: 10.0,
+            setpoint_speed: f64::INFINITY,
+            actual_consumption: 9.0,
+            actual_speed: 11.0,
+            lever: 0.5,
+            ..Default::default()
+        };
+
+        sys.do_step(0.0, 1.0);
+        assert_eq!(sys.mode, 1);
+        assert!(sys.lever > 0.5);
+    }
+}
